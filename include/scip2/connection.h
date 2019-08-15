38bca8041be351445f0c710bd71cfe71ce875b8e
Atsushi Watanabe <atsushi.w@ieee.org>
2019-08-15 11:43:18 +0000

    Rebase from 'release/melodic/urg_stamped'

--- include/scip2/connection.h
+++ include/scip2/connection.h
@@ -22,7 +22,7 @@
 
 #include <string>
 
-#include <old_boost_fix.h>
+#include <scip2/logger.h>
 
 namespace scip2
 {
@@ -110,7 +110,7 @@ protected:
   {
     if (!error)
     {
-      std::cerr << "Watchdog timeout" << std::endl;
+      logger::fatal() << "Watchdog timeout" << std::endl;
       close();
     }
   }
@@ -120,7 +120,7 @@ protected:
     const auto time_read = boost::posix_time::microsec_clock::universal_time();
     if (error)
     {
-      std::cerr << "Receive error" << std::endl;
+      logger::fatal() << "Receive error" << std::endl;
       close();
       return;
     }
@@ -133,7 +133,7 @@ protected:
     const auto time_send = boost::posix_time::microsec_clock::universal_time();
     if (error)
     {
-      std::cerr << "Send error" << std::endl;
+      logger::fatal() << "Send error" << std::endl;
       close();
       return;
     }
@@ -151,7 +151,7 @@ protected:
   {
     if (error)
     {
-      std::cerr << "Connection error" << std::endl;
+      logger::fatal() << "Connection error" << std::endl;
       close();
       return;
     }
@@ -163,7 +163,7 @@ protected:
   {
     if (!error)
     {
-      std::cerr << "Connection timeout" << std::endl;
+      logger::fatal() << "Connection timeout" << std::endl;
       close();
       return;
     }
